1b33f097625232d903fea6042ebc20a249baa86b
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-23 19:18:18 +0200

    Bugfix

--- src/Osynapsy/Bcl4/DataGrid/DataGrid.php
+++ src/Osynapsy/Bcl4/DataGrid/DataGrid.php
@@ -39,8 +39,8 @@ class DataGrid extends Component
     {
         parent::__construct('div', $name);
         $this->setClass('bcl-datagrid');
-        $this->requireCss('assets/Bcl/DataGrid/style.css');
-        $this->requireJs('assets/Bcl/DataGrid/script.js');
+        $this->requireCss('assets/Bcl4/DataGrid/style.css');
+        $this->requireJs('assets/Bcl4/DataGrid/script.js');
     }
 
     /**
