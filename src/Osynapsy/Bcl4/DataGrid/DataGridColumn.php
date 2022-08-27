00d78bb686ef81c52f4423a0e99a1421fcef35c8
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-27 09:47:13 +0200

    Added license disclaimer

--- src/Osynapsy/Bcl4/DataGrid/DataGridColumn.php
+++ src/Osynapsy/Bcl4/DataGrid/DataGridColumn.php
@@ -1,12 +1,22 @@
 <?php
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Osynapsy\Bcl4\DataGrid;
 
 use Osynapsy\Html\Tag;
 
 /**
- * Description of DataGrid2Column
+ * Description of DataGridColumn
  *
- * @author pietr
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class DataGridColumn
 {
